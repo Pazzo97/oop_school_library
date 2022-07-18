2c093b1d5f6349745f3b44c9c6a84cb40f59b9d4
Pazzo97 <pazzomuk@gmail.com>
2022-07-18 17:57:46 +0200

    Add name in super

--- student.rb
+++ student.rb
@@ -3,8 +3,7 @@ require_relative './person'
 class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     @classroom = classroom
-    @name = name
-    super(age, parent_permission: parent_permission)
+    super(age, name, parent_permission: parent_permission)
   end
 
   def play_hooky
--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,7 @@ require_relative './person'
 class Teacher < Person
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     @specialization = specialization
-    @name = name
-    super(age, parent_permission: parent_permission)
+    super(age, name, parent_permission: parent_permission)
   end
 
   def can_use_services?
