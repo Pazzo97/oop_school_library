74c8d30a8a9295fb57d3e85f3ac5cdf31b102b90
Pazzo97 <pazzomuk@gmail.com>
2022-07-21 13:33:19 +0200

    modify rentals

--- rental.rb
+++ rental.rb
@@ -4,9 +4,17 @@ class Rental
 
   def initialize(date, person, book)
     @date = date
-    @person = person
-    person.rentals << self
+    add_person(person)
+    add_book(book)
+  end
+
+  def add_book(book)
     @book = book
     book.rentals << self
+  end
+
+  def add_person(person)
+    @person = person
+    person.rentals << self
   end
 end
